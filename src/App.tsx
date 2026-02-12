fb46a2e98b809a9fc65e964bdfe3858f67c0b58b
Paul Bond <git@paulsbond.co.uk>
2026-02-12 21:19:49 +0000

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router";
+
+import { Home } from "./pages/Home";
+
+import "./App.css";
+import "material-symbols/outlined.css";
+
+const root = document.getElementById("root")!;
+
+createRoot(root).render(
+  <HashRouter>
+    <StrictMode>
+      <div className="flex flex-col gap-4 p-4 text-gray-900">
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </div>
+    </StrictMode>
+  </HashRouter>,
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <main className="m-auto flex max-w-7xl flex-col gap-4">
+      <h1 className="mb-1 text-center text-3xl font-bold">Morris</h1>
+    </main>
+  );
+}
