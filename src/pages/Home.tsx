d1974fa7e6ec1331dd48e3f4fadbe59a833e1f1b
Paul Bond <git@paulsbond.co.uk>
2026-02-12 22:27:26 +0000

    Margin bottom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -68,7 +68,7 @@ function Row({ dance }: { dance: Dance }) {
 
 export function Home() {
   return (
-    <main className="m-auto flex max-w-7xl flex-col gap-4">
+    <main className="m-auto mb-6 flex max-w-7xl flex-col gap-4">
       <h1 className="mb-1 text-center text-3xl font-bold">Morris</h1>
 
       {traditions.map(({ tradition, dances }) => (
