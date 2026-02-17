2380d402622fe6efe3e9e74749b0d315e24ef0b2
Paul Bond <git@paulsbond.co.uk>
2026-02-17 22:44:19 +0000

    PWA updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Morris",
+        short_name: "Morris",
+        description: "My repository of morris dances",
+        theme_color: "#355143",
+        background_color: "#355143",
+        icons: [
+          {
+            src: "icon-192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "icon-512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+          {
+            src: "icon-mask.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
