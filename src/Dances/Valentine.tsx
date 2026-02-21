a578e28c8d48217236b44eecc0d3e95bfee9e0ed
Paul Bond <git@paulsbond.co.uk>
2026-02-21 20:35:49 +0000

    Valentine

--- src/Dances/Valentine.tsx
+++ src/Dances/Valentine.tsx
@@ -4,4 +4,16 @@ export const valentine: Dance = {
   name: "Valentine",
   key: "valentine",
   tradition: "Fieldtown",
+  info: ["Hankies", "Double step", "Outside foot start"],
+  figures: [
+    "Foot up",
+    "Chorus (salutes)",
+    "Half gyp",
+    "Chorus (galleys)",
+    "Back to back",
+    "Chorus (beetle crushers)",
+    "Fieldtown rounds",
+    "Chorus (upright RTBs)",
+    "Hay",
+  ],
 };
