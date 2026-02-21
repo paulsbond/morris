462e8467c14c34f229f632f0db9d96e5c0a05149
Paul Bond <git@paulsbond.co.uk>
2026-02-21 20:23:20 +0000

    Vandals of Hammerwich

--- src/Dances/VandalsOfHammerwich.tsx
+++ src/Dances/VandalsOfHammerwich.tsx
@@ -4,4 +4,21 @@ export const vandalsOfHammerwich: Dance = {
   name: "Vandals of Hammerwich",
   key: "vandals-of-hammerwich",
   tradition: "Lichfield",
+  info: ["Long stick", "Double step", "Right foot start"],
+  song: [
+    "Oh, will she, won't she, will she, won't she, come to the fair?",
+    "Will she, won't she, will she, won't she, come to the fair?",
+  ],
+  figures: [
+    "Split rounds singing",
+    "Chorus",
+    "Heading up",
+    "Chorus",
+    "Back to back into line",
+    "Chorus",
+    "Heading down",
+    "Chorus",
+    "Lichfield hay",
+    "Chorus",
+  ],
 };
