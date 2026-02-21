4eb3dfe979d881c1f7cbe34ab8e3852371ba4dec
Paul Bond <git@paulsbond.co.uk>
2026-02-21 13:30:07 +0000

    Video width

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,7 @@
 export function Video(props: { id: string }) {
   return (
     <iframe
-      className="aspect-video max-w-lg"
+      className="aspect-video w-lg max-w-full"
       src={`https://www.youtube-nocookie.com/embed/${props.id}`}
       allowFullScreen
     ></iframe>
