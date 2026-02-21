bc3b5ef2e9497cacc1ec8f51410eb0b526f57da8
Paul Bond <git@paulsbond.co.uk>
2026-02-21 20:45:47 +0000

    William and Nancy

--- src/Dances/WilliamAndNancy.tsx
+++ src/Dances/WilliamAndNancy.tsx
@@ -4,4 +4,16 @@ export const williamAndNancy: Dance = {
   name: "William and Nancy",
   key: "william-and-nancy",
   tradition: "Bledington",
+  info: ["Hankies", "Double step", "Outside foot start"],
+  figures: [
+    "Foot up",
+    "Chorus (salutes)",
+    "Half gyp",
+    "Chorus (flurry capers)",
+    "Whole gyp",
+    "Chorus (beetle crushers)",
+    "Rounds",
+    "Chorus (RTBs)",
+    "Hay",
+  ],
 };
