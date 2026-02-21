6abd7f932e49eb9262d6068f3a4e2ac11dd06f78
Paul Bond <git@paulsbond.co.uk>
2026-02-21 20:39:55 +0000

    Banks of the Dee

--- src/Dances/BanksOfTheDee.tsx
+++ src/Dances/BanksOfTheDee.tsx
@@ -4,4 +4,16 @@ export const banksOfTheDee: Dance = {
   name: "Banks of the Dee",
   key: "banks-of-the-dee",
   tradition: "Fieldtown",
+  info: ["Hankies", "Double step", "Outside foot start"],
+  figures: [
+    "Foot up",
+    "Chorus (salutes)",
+    "Half gyp",
+    "Chorus (galleys)",
+    "Back to back",
+    "Chorus (beetle crushers)",
+    "Fieldtown rounds",
+    "Chorus (upright RTBs)",
+    "Hay",
+  ],
 };
