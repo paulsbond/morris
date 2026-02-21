77c12be5b33dd89d4e8ba0ef0e24382b604bfc24
Paul Bond <git@paulsbond.co.uk>
2026-02-21 20:43:05 +0000

    Jenny Lind

--- src/Dances/JennyLind.tsx
+++ src/Dances/JennyLind.tsx
@@ -4,4 +4,17 @@ export const jennyLind: Dance = {
   name: "Jenny Lind",
   key: "jenny-lind",
   tradition: "Lichfield",
+  info: ["Long/short stick", "Double step", "Right foot start"],
+  figures: [
+    "Split rounds",
+    "Chorus",
+    "Heading up",
+    "Chorus",
+    "Back to back into line",
+    "Chorus",
+    "Heading down",
+    "Chorus",
+    "Lichfield hay",
+    "Chorus",
+  ],
 };
