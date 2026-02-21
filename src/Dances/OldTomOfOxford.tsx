a7f7e6b36548d49cbce6f2a5b569728bf2412257
Paul Bond <git@paulsbond.co.uk>
2026-02-21 20:27:06 +0000

    Old Tom of Oxford

--- src/Dances/OldTomOfOxford.tsx
+++ src/Dances/OldTomOfOxford.tsx
@@ -4,4 +4,15 @@ export const oldTomOfOxford: Dance = {
   name: "Old Tom of Oxford",
   key: "old-tom-of-oxford",
   tradition: "Bampton",
+  info: ["Hankies", "Double step", "Right foot start"],
+  figures: [
+    "Foot up",
+    "Chorus",
+    "Half gyp",
+    "Chorus",
+    "Whole gyp",
+    "Chorus",
+    "Rounds",
+    "Chorus",
+  ],
 };
