928fe7f57f8c028490eccdaa148c5f2074444585
Paul Bond <git@paulsbond.co.uk>
2026-02-21 20:26:34 +0000

    The Rose Tree

--- src/Dances/TheRoseTree.tsx
+++ src/Dances/TheRoseTree.tsx
@@ -4,4 +4,14 @@ export const theRoseTree: Dance = {
   name: "The Rose Tree",
   key: "the-rose-tree",
   tradition: "Bampton",
+  info: ["Hankies", "Double step", "Right foot start"],
+  figures: [
+    "Foot up",
+    "Chorus (back to place)",
+    "Half gyp",
+    "Chorus (change places)",
+    "Whole gyp",
+    "Chorus (change places)",
+    "Rounds",
+  ],
 };
