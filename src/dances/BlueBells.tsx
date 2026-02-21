179bb1982ee4f504ef560d6b5f10737635d896ff
Paul Bond <git@paulsbond.co.uk>
2026-02-21 13:17:30 +0000

    BlueBells update

--- src/dances/BlueBells.tsx
+++ src/dances/BlueBells.tsx
@@ -1,8 +1,47 @@
+function Heading2({ children }: { children: React.ReactNode }) {
+  return <h2 className="mb-1 text-2xl font-bold">{children}</h2>;
+}
+
 export function BlueBells() {
   return (
     <>
-      <p>Some information about Blue Bells of Scotland.</p>
-      <p>Maybe a video...</p>
+      <div className="flex justify-center gap-4">
+        <p>Long stick</p>
+        <p>|</p>
+        <p>Double step</p>
+        <p>|</p>
+        <p>Right foot start</p>
+      </div>
+      <Heading2>Lyrics</Heading2>
+      <p className="italic">
+        Oh where, tell me where, has my highland laddie gone?
+        <br />
+        He's gone to the war with his frilly knickers on.
+      </p>
+      <Heading2>Figures</Heading2>
+      <ol className="list-inside">
+        <li className="font-bold">Walk around singing</li>
+        <li className="font-bold">Foot up</li>
+        <li className="italic">Chorus</li>
+        <li className="font-bold">Half gyp</li>
+        <li className="italic">Chorus</li>
+        <li className="font-bold">Process down (tops down)</li>
+        <li className="italic">Chorus</li>
+        <li className="font-bold">Process up (bottoms up)</li>
+        <li className="italic">Chorus</li>
+        <li className="font-bold">Hands around</li>
+        <li className="italic">Chorus</li>
+        <li className="font-bold">Adderbury Hay</li>
+        <li className="italic">Chorus</li>
+        <li className="font-bold">Doubles</li>
+      </ol>
+      <iframe
+        className="aspect-video max-w-lg"
+        src="https://www.youtube-nocookie.com/embed/L0GF4xRvB1Y?si=a92IEH6fz0IT60LY"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
     </>
   );
 }
--- src/pages/Dance.tsx
+++ src/pages/Dance.tsx
@@ -3,7 +3,7 @@ import { Dance as DanceType } from "../dances";
 export function Dance({ dance }: { dance: DanceType }) {
   return (
     <main className="m-auto mb-6 flex max-w-7xl flex-col gap-4">
-      <header className="text-center">
+      <header className="mb-1 text-center">
         <h1 className="mb-1 text-3xl font-bold">{dance.name}</h1>
         <p className="text-lg">{dance.tradition}</p>
       </header>
