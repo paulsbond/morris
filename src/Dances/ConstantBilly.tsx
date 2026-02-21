b87d836009bc38e6bb9a9adb11fd838956a8e687
Paul Bond <git@paulsbond.co.uk>
2026-02-21 20:41:54 +0000

    Constant Billy

--- src/Dances/ConstantBilly.tsx
+++ src/Dances/ConstantBilly.tsx
@@ -4,4 +4,15 @@ export const constantBilly: Dance = {
   name: "Constant Billy",
   key: "constant-billy",
   tradition: "Headington",
+  info: ["Short sticks", "Double step", "Right foot start"],
+  figures: [
+    "Foot up",
+    "Chorus",
+    "Back to back",
+    "Chorus",
+    "Crossover",
+    "Chorus",
+    "Hay",
+    "Chorus",
+  ],
 };
