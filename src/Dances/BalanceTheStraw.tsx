4497a9741741f472c3aab0dc87a8aeeeb83ce2cf
Paul Bond <git@paulsbond.co.uk>
2026-02-21 20:39:00 +0000

    Balance the Straw

--- src/Dances/BalanceTheStraw.tsx
+++ src/Dances/BalanceTheStraw.tsx
@@ -4,4 +4,16 @@ export const balanceTheStraw: Dance = {
   name: "Balance the Straw",
   key: "balance-the-straw",
   tradition: "Fieldtown",
+  info: ["Long stick", "Double step", "Outside foot start"],
+  figures: [
+    "Foot up",
+    "Chorus",
+    "Half gyp",
+    "Chorus",
+    "Back to back",
+    "Chorus",
+    "Fieldtown rounds",
+    "Chorus",
+    "Hay",
+  ],
 };
