0e10e5f2e37a335694478d9f7d0f3b3ed896f60b
Paul Bond <git@paulsbond.co.uk>
2026-03-03 21:26:23 +0000

    Jockey to the Fair info

--- src/Dances/JockeyToTheFair.tsx
+++ src/Dances/JockeyToTheFair.tsx
@@ -4,5 +4,6 @@ export const jockeyToTheFair: Dance = {
   name: "Jockey to the Fair",
   key: "jockey-to-the-fair",
   tradition: "Brackley",
+  info: ["Bagged hankies", "Single step", "Right foot start"],
   videos: ["nBDvTKl6ixo"],
 };
